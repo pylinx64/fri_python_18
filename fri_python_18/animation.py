90371f62fbfa85166be4dcd32a02b21d778d6e2e
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-26 20:06:44 +0200

    Add files via upload

---
+++ fri_python_18/animation.py
@@ -0,0 +1,35 @@
+import pygame 
+
+pygame.init()
+
+# цвета
+black = (0, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+red = (250, 0, 250)
+
+# размер экрана
+size = [1280, 768]
+screen = pygame.display.set_mode(size)
+
+# заголовок экрана
+pygame.display.set_caption('Название игры 2077')
+
+# FPS
+clock = pygame.time.Clock()
+
+#------------------------------основной цикл----------------------------
+shutdown = True
+while shutdown == True:
+	# СОБЫТИЯ НАЧАЛО
+	# СОБЫТИЯ КОНЕЦ
+	
+	
+	# РИСОВАНИЕ НАЧАЛО
+	# РИСОВАНИЕ КОНЕЦ
+	
+	
+	# ЛОГИКА НАЧАЛО
+	# ЛОГИКА КОНЕЦ
+	
+	
